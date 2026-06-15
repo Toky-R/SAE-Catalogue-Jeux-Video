8277a7b221066047b138c5076a30f2f0a1b1e6a7
KarimAlsh <kaarim.alsh12@gmail.com>
2026-06-15 09:30:32 +0200

    Modification du chemin d'acces

--- api/genres.php
+++ api/genres.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-require_once '/db.php';
+require_once '../config/db.php';
 echo json_encode($pdo->query("SELECT * FROM genre ORDER BY nom")->fetchAll());
